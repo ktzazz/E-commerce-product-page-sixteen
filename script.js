9e79aff43881d50541f7f2c449574e045f92f77f
ktzazz <155502502+ktzazz@users.noreply.github.com>
2025-08-05 18:48:37 -0600

    lightbox 1

--- script.js
+++ script.js
@@ -25,9 +25,64 @@ document.addEventListener("DOMContentLoaded", function () {
   // lightbox gallery
   const mainImagesContainer = document.querySelector(".main-image");
   const mainImages = document.querySelectorAll(".main-image img");
-  const thumbnails = document.querySelectorAll(".thumbnail img");
+  const mainThumbnails = document.querySelectorAll(".thumbnail img");
+  const lightboxThumbnailsContainer = document.querySelector(
+    ".lightbox-thumbnails"
+  );
+  const lightboxContainer = document.querySelector("#lightbox-modal");
   const prevArrow = document.querySelector(".prev-arrow");
   const nextArrow = document.querySelector(".next-arrow");
 
   let currentImageIndex = 0;
+
+  function updateGallery(newIndex) {
+    if (newIndex >= mainImages.length) {
+      newIndex = 0;
+    } else if (newIndex < 0) {
+      newIndex = mainImages.length - 1;
+    }
+    currentImageIndex = newIndex;
+
+    mainImages.forEach((img, index) => {
+      if (index === currentImageIndex) {
+        img.classList.remove("inactive");
+        img.classList.add("active");
+      } else {
+        img.classList.remove("active");
+        img.classList.add("inactive");
+      }
+    });
+
+    mainThumbnails.forEach((thumbnail, index) => {
+      if (index === currentImageIndex) {
+        thumbnail.classList.add("thumbnail-active");
+      } else {
+        thumbnail.classList.remove("thumbnail-active");
+      }
+    });
+  }
+
+  prevArrow.addEventListener("click", () =>
+    updateGallery(currentImageIndex - 1)
+  );
+  nextArrow.addEventListener("click", () =>
+    updateGallery(currentImageIndex + 1)
+  );
+
+  mainThumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", () => updateGallery(index));
+  });
+
+  const onDesktop = window.matchMedia("(min-width: 768px)");
+
+  if (onDesktop.matches) {
+    lightboxContainer.style.display = "block";
+  }
+
+  mainThumbnails.forEach((thumbnail) => {
+    //clones the thumbnail element so i don't copy/paste on the html
+    const lightboxThumbnail = thumbnail.cloneNode(true);
+    //adds the lighbox style class
+    lightboxThumbnail.classList.add("lightbox-thumbnail-img");
+  });
 });
